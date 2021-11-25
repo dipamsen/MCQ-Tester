f048af7978ef5c63edef3cff670befbe66bc4d37
Fun Planet <dipamdiptam@gmail.com>
2021-11-25 20:58:25 +0530

    gen subj

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+const files = {
+  Eng_T1: "English Term I",
+  SST_T1: "Social Science Term I",
+};
 let score = 0;
 let index = 0;
 let originalIndex = null;
-let updateScores = true;
-async function readFile() {
-  return await fetch("100qeng.txt").then((res) => res.text());
+const settings = {
+  scoring: true,
+  file: "Eng_T1",
+};
+async function readFile(file) {
+  return await fetch(file + ".json").then((res) => res.json());
 }
 
 async function main() {
   // parse settings from url param
-  const settings = new URLSearchParams(window.location.search);
-  updateScores = settings.get("score") !== "false";
-  if (!updateScores) document.querySelector(".score").style.display = "none";
+  updateSettings();
+  if (!settings.scoring)
+    document.querySelector(".score").style.display = "none";
   const cache = JSON.parse(localStorage.getItem("MCQ__TESTER__DATA"));
 
-  const file = (await readFile()).replace(/[\r\n]+/g, " ");
-  // regex match for questions
-  const regex =
-    /(\d+)\.\s+(.*?)\s+\(a\)\s+(.*?)\s+\(b\)\s+(.*?)\s+\(c\)\s+(.*?)\s+\(d\)\s+(.*?)\s+Ans\.\s+\((.)\)/gm;
-  const questions = [...file.matchAll(regex)];
+  const questions = await readFile(settings.file);
 
   if (cache && cache.index) {
     index = cache.index;
@@ -38,20 +41,24 @@ async function main() {
 main();
 
 function setupQuestion(question) {
-  document.querySelector(".question").textContent = question[2];
-  document.querySelector(".option-a").textContent = "(a) " + question[3];
-  document.querySelector(".option-b").textContent = "(b) " + question[4];
-  document.querySelector(".option-c").textContent = "(c) " + question[5];
-  document.querySelector(".option-d").textContent = "(d) " + question[6];
-  const correct = question[7];
+  document.querySelector(".question").textContent = question.question;
+  document.querySelector(".option-a").textContent =
+    "(a) " + question.options[0];
+  document.querySelector(".option-b").textContent =
+    "(b) " + question.options[1];
+  document.querySelector(".option-c").textContent =
+    "(c) " + question.options[2];
+  document.querySelector(".option-d").textContent =
+    "(d) " + question.options[3];
+  const correct = question.correctOpt;
   const clickListener = (e) => {
     index = (index % 100) + 1;
     if (!e.target.classList.contains(`option-${correct}`)) {
       e.target.classList.add("incorrect");
-      if (updateScores) wrongAnswer();
+      if (settings.scoring) wrongAnswer();
       else document.querySelector("#next").disabled = false;
     } else {
-      if (updateScores) correctAnswer();
+      if (settings.scoring) correctAnswer();
       document.querySelector("#next").disabled = false;
     }
     document.querySelector(`.option-${correct}`).classList.add("correct");
@@ -100,4 +107,11 @@ function showModal() {
   // set score-final
   document.querySelector("#score-final").textContent = score;
   document.querySelector(".modal").style.display = "flex";
+}
+
+function updateSettings() {
+  const urlParams = new URLSearchParams(window.location.search);
+  settings.scoring = urlParams.get("score") !== "false";
+  settings.file = urlParams.get("file") || "Eng_T1";
+  document.querySelector(".title").textContent = files[settings.file];
 }
